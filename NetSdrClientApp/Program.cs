3e94db6d321800f110243116485819ed733096c4
kustiomiend-boh <steamgamemyname@gmail.com>
2025-12-01 19:26:24 +0200

    Lab8.2

--- NetSdrClientApp/Program.cs
+++ NetSdrClientApp/Program.cs
@@ -1,33 +1,16 @@
-﻿using NetSdrClientApp;
-using NetSdrClientApp.Networking;
-using System;
-using System.Threading.Tasks;
+﻿using System;
 
-namespace NetSdrClientApp
+namespace EchoTcpServer
 {
-    public static class Program
+    class Program
     {
-        public static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine(@"Usage: C-connect, D-disconnect, F-freq, S-Start/Stop, Q-quit");
-
-            // Налаштування залежностей
-            var tcpClient = new TcpClientWrapper("127.0.0.1", 5000);
-            var udpClient = new UdpClientWrapper(60000);
-            var netSdr = new NetSdrClient(tcpClient, udpClient);
+            Console.WriteLine("Цей проект залишено для сумісності.");
+            Console.WriteLine("Основна логіка сервера перенесена у NetSdrClientApp для тестування.");
             
-            // Використовуємо наш новий процесор
-            var processor = new CommandProcessor(netSdr);
-
-            bool isRunning = true;
-            while (isRunning)
-            {
-                // Цей рядок неможливо протестувати, але це єдине, що залишилось не покритим!
-                var key = Console.ReadKey(intercept: true).Key;
-                
-                // Вся логіка тепер тут, і вона покрита тестами:
-                isRunning = await processor.HandleKeyAsync(key);
-            }
+            // Щоб консоль не закривалась одразу
+            Console.ReadLine();
         }
     }
 }
